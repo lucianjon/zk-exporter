f3efb168008e385138e0c58b1313a6547ef68ce5
Lucian Jones <lucianm.jones@gmail.com>
2017-09-17 18:53:14 +1200

    update instance label and some help strings

--- metrics.go
+++ metrics.go
@@ -52,92 +52,92 @@ func initMetrics() map[string]*prometheus.GaugeVec {
 	metrics[zkAvgLatency] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkAvgLatency,
 		Help: "Average Latency for ZooKeeper network requests.",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkMinLatency] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkMinLatency,
 		Help: "Minimum latency for Zookeeper network requests.",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkMaxLatency] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkMaxLatency,
 		Help: "Maximum latency for ZooKeeper network requests",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkPacketsReceived] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkPacketsReceived,
-		Help: "Number of network packets received by ZooKeeper instance.",
-	}, []string{"instance"})
+		Help: "Number of network packets received by the ZooKeeper instance.",
+	}, []string{"zk_instance"})
 
 	metrics[zkPacketsSent] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkPacketsSent,
-		Help: "Number of network packets sent by ZooKeeper instance.",
-	}, []string{"instance"})
+		Help: "Number of network packets sent by the ZooKeeper instance.",
+	}, []string{"zk_instance"})
 
 	metrics[zkNumAliveConnections] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkNumAliveConnections,
 		Help: "Number of currently alive connections to the ZooKeeper instance.",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkOutstandingRequests] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkOutstandingRequests,
 		Help: "Number of requests currently waiting in the queue.",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkZnodeCount] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkZnodeCount,
 		Help: "Znode count",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkWatchCount] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkWatchCount,
 		Help: "Watch count",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkEphemeralsCount] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkEphemeralsCount,
 		Help: "Ephemerals Count",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkApproximateDataSize] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkApproximateDataSize,
 		Help: "Approximate data size",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkOpenFileDescriptorCount] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkOpenFileDescriptorCount,
 		Help: "Number of currently open file descriptors",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkMaxFileDescriptorCount] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkMaxFileDescriptorCount,
 		Help: "Maximum number of open file descriptors",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkServerState] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkServerState,
-		Help: "test help",
-	}, []string{"instance"})
+		Help: "Current state of the zk instance: 1 = follower, 2 = leader, 3 = standalone, -1 if unknown",
+	}, []string{"zk_instance"})
 
 	metrics[zkFollowers] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkFollowers,
 		Help: "Leader only: number of followers.",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkSyncedFollowers] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkSyncedFollowers,
 		Help: "Leader only: number of followers currenty in sync",
-	}, []string{"instance"})
+	}, []string{"zk_instance"})
 
 	metrics[zkPendingSyncs] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkPendingSyncs,
-		Help: "pending syncs",
-	}, []string{"instance"})
+		Help: "Current number of pending syncs",
+	}, []string{"zk_instance"})
 
 	metrics[zkOK] = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Name: zkOK,
-		Help: "is zk ok",
-	}, []string{"instance"})
+		Help: "Is ZooKeeper currently OK",
+	}, []string{"zk_instance"})
 
 	for _, metric := range metrics {
 		prometheus.MustRegister(metric)
